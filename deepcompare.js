4412e3aa45a210ca8616834dd171758eb8764c0f
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-16 09:38:57 -0500

    Update deepcompare.js

--- deepcompare.js
+++ deepcompare.js
@@ -1,8 +1,18 @@
 function deepcompare(a,b){
   var gettype_a = typeof a;
   var gettype_b = typeof b;
-  if(gettype_a === gettype_b)
-    return true;
+  if(gettype_a === gettype_b){
+     var getkeys_a = Object.keys(a);
+     var getkeys_b = Object.keys(b);
+     for(var i in getkeys_a){
+      for(var j in getkeys_b){
+        if((i==j) && (getkeys_a[i] === getkeys_b[j]))
+            return true;
+        return false;
+      }
+     }
+  }
+    
   else
     return false;
 }
