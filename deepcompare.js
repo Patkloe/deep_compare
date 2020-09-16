a48e1a2dbe2a758b0ec30d5f5c61d6f11a499273
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-16 10:15:08 -0500

    Update deepcompare.js

--- deepcompare.js
+++ deepcompare.js
@@ -5,10 +5,23 @@ function deepcompare(a,b){
      var getkeys_a = Object.keys(a);
      var getkeys_b = Object.keys(b);
      for(var i in getkeys_a){
+         var obj_a = typeof getkeys_a[i]; 
+         //object
       for(var j in getkeys_b){
-        if((i==j) && (getkeys_a[i] === getkeys_b[j]))
-            return true;
-        return false;
+         var obj_b = typeof getkeys_b[j];
+         if(i === j){  // a modifier ajouter comparaison proprietes des objets ici
+          if ((obj_a!= "object") && (obj_a!= "object")){
+           if(getkeys_a[i] === getkeys_b[j])
+             return true;
+           else 
+             return false;
+          }
+          else
+             deepcompare(getkeys_a[i],getkeys_b[j])
+         }
+         else
+           return false;
+        
       }
      }
   }
@@ -17,5 +30,6 @@ function deepcompare(a,b){
     return false;
 }
 var a = { 1:2, 3:4};
-var b = {5:6, 7:8};
+var b = {1:2, 3:5};
     deepcompare(a,b);
+
