b798a047b5c9210b1b76e9565af3fba2d13721b2
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-17 07:31:50 -0500

    Update deepcompare.js

--- deepcompare.js
+++ deepcompare.js
@@ -2,6 +2,7 @@ function deepcompare(a,b){
  var ver;
  var get_type_a = typeof a;
  var get_type_b = typeof b
+if((get_type_a == "object") &&  (get_type_b == "object")){
  if(get_type_a === get_type_b){
    var get_keys_a = Object.keys(a);
    var get_keys_b = Object.keys(b);
@@ -20,11 +21,22 @@ function deepcompare(a,b){
           ver = false;
      } // fin for i
    }// fin for j
-  console.log(ver);
+  //console.log(ver);
  }//contraire meme type
- else
- return false;
+ else if(a!== b)
+   ver = false;
 }
-var x = {1:2, 3:4,5:6, 5:6};
-var y = {1:2, 3:4,5:6, 5:6};
+else if((get_type_a != "object") ||  (get_type_b != "object")){
+   ver = false;
+}
+else if((get_type_a != "object") &&  (get_type_b != "object")){
+     if(a == b)
+       ver = true;
+     else
+       ver = false;
+}
+    console.log(ver);
+}
+var x = {1:2, 3:4,5:6, 5:6, 9:{1:2, 3:4,5:6, 7:{5:8, 7:9}}};
+var y = {1:2, 3:4,5:6, 5:6, 9:{1:2, 3:4,5:6, 7:{5:8, 7:9}}};
 deepcompare(x,y);
