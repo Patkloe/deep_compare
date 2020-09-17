ed01e220e61b50cf4d4c9d65aef68dc30d7b8086
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-16 21:38:20 -0500

    Update deepcompare.js

--- deepcompare.js
+++ deepcompare.js
@@ -1,35 +1,30 @@
 function deepcompare(a,b){
-  var gettype_a = typeof a;
-  var gettype_b = typeof b;
-  if(gettype_a === gettype_b){
-     var getkeys_a = Object.keys(a);
-     var getkeys_b = Object.keys(b);
-     for(var i in getkeys_a){
-         var obj_a = typeof getkeys_a[i]; 
-         //object
-      for(var j in getkeys_b){
-         var obj_b = typeof getkeys_b[j];
-         if(i === j){  // a modifier ajouter comparaison proprietes des objets ici
-          if ((obj_a!= "object") && (obj_a!= "object")){
-           if(getkeys_a[i] === getkeys_b[j])
-             return true;
-           else 
-             return false;
-          }
-          else
-             deepcompare(getkeys_a[i],getkeys_b[j])
-         }
-         else
-           return false;
-        
-      }
-     }
-  }
-    
-  else
-    return false;
+ var ver;
+ var get_type_a = typeof a;
+ var get_type_b = typeof b
+ if(get_type_a === get_type_b){
+   var get_keys_a = Object.keys(a);
+   var get_keys_b = Object.keys(b);
+   for(i in get_keys_a){
+     var type_a = typeof get_keys_a[i];
+     for(j in get_keys_b){
+      var type_b = typeof get_keys_b[j];
+       if((i === j) && (type_a === "object") && (type_b === "object")){
+          deepcompare(get_keys_a[i],get_keys_b[j]);
+       }
+       else if((i === j) && (type_a === type_b) && (get_keys_a[i] == get_keys_b[j])){
+          alert(type_a + type_b);
+          ver = true;
+       }
+       else
+          ver = false;
+     } // fin for i
+   }// fin for j
+  console.log(ver);
+ }//contraire meme type
+ else
+ return false;
 }
-var a = { 1:2, 3:4};
-var b = {1:2, 3:5};
-    deepcompare(a,b);
-
+var x = {1:2, 3:4,5:6, 5:6};
+var y = {1:2, 3:4,5:6, 5:6};
+deepcompare(x,y);
