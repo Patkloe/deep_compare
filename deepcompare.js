bc07ec18f26679ce09dd7c92c0c719cd6d5d1b1d
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-20 17:35:28 -0500

    Update deepcompare.js

--- deepcompare.js
+++ deepcompare.js
@@ -2,18 +2,25 @@ function deepcompare(a,b){
 var type_a = typeof a;
 var type_b = typeof b;
 var ver = false;
-var typed;
+var typea;
+var typeb;
  if((type_a === "object")&&(type_b === "object")){
     alert("object 1");
     for(var i in a){
+     var c = i;
+     typea = typeof a[c];
      for(var j in b){
-       typed = typeof a[i];
-       alert(typed);
-      if((i === j) && (typeof a[i] === "object") && (typeof b[j] === "object")){
+       var d = j
+       typeb = typeof b[d];
+       alert("a type :"+ " " + typea + " " + "b type : " + " " + typeb);
+      if((c === d) && (typea === "object") && (typeb === "object") ){
+                  alert("trouve");
                   deepcompare(a[i],b[j]);
       }
-      else if((i === j) && (typeof a[i] !== "object") && (typeof b[j] !== "object") && (a[i] === b[j]))
+      else if((i === j) && (typeof a[i] !== "object") && (typeof b[j] !== "object") && (a[i] === b[j])){
+          alert(true);
           ver = true;
+      }
       else if((i === j) && (typeof a[i] !== "object") && (typeof b[j] !== "object") && (a[i] !== b[j])){
           alert(false);
           ver = false;
@@ -32,4 +39,4 @@ deepcompare(1,2);
 deepcompare("a","b");
 deepcompare("a","a");
 deepcompare(1,1);
-deepcompare({1:2, 3:{2:5, 6:7}},{1:2, 4:{2:5, 6:8}});
+deepcompare({1:2, 3:{2:5, 6:7}},{1:2, 3:{2:5, 6:8}});
