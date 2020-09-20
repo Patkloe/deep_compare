68af7968c9496a90d7a0c80d2510e595c510d11e
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-19 19:36:35 -0500

    Update deepcompare.js

--- deepcompare.js
+++ deepcompare.js
@@ -1,33 +1,35 @@
 function deepcompare(a,b){
- var ver = false;
- var get_type_a = typeof a;
- var get_type_b = typeof b
- if((get_type_a === "object") && (get_type_b === "object")){
-  var getkeys_a = Object.keys(a);
-  var getkeys_b = Object.keys(b);
-  for(var i of getkeys_a){
-      var typekeys_a = typeof getkeys_a[i];
-        alert("a type, i : " + " " + typekeys_a);
-    for(var j of getkeys_b){
-      var typekeys_b = typeof getkeys_b[j];
-         alert("a type, j : " + " " + typekeys_b);
-      if((i === j) && (getkeys_a[i] === "object") && (getkeys_b[j] === "object")){
-          alert("i :" + " " + i + " " + "val :" + " " + getkeys_a[i] + " " + " j :" + " " + j + " " + "val :" + " " + getkeys_b[j]);
-          deepcompare(getkeys_a[i],getkeys_b[j]);
-       }
-      else if((i === j) && (getkeys_a[i] === getkeys_b[j]))
+var type_a = typeof a;
+var type_b = typeof b;
+var ver = false;
+var typed;
+ if((type_a === "object")&&(type_b === "object")){
+    alert("object 1");
+    for(var i in a){
+     for(var j in b){
+       typed = typeof a[i];
+       alert(typed);
+      if((i === j) && (typeof a[i] === "object") && (typeof b[j] === "object")){
+                  deepcompare(a[i],b[j]);
+      }
+      else if((i === j) && (typeof a[i] !== "object") && (typeof b[j] !== "object") && (a[i] === b[j]))
           ver = true;
-      else if((i === j) && (getkeys_a[i] !== getkeys_b[j]))
+      else if((i === j) && (typeof a[i] !== "object") && (typeof b[j] !== "object") && (a[i] !== b[j])){
+          alert(false);
           ver = false;
-     alert( "i :" + i + " " + "j:" + " " + j );
-    }// fin for j
-  } // fin for i
-    //alert("travail a faire ");
- }//contraire meme type
- else if(a === b)
-  ver = true;
+          break;
+      }
+     } // fin j
+    }// fin i
+ }// fin if type object
+ else if(((type_a !== "object") && (type_b !== "object")) && (a !== b))
+     ver = false
+ else if(((type_a !== "object") && (type_b !== "object")) && (a == b)) 
+     ver = true;
  console.log(ver);
 }
-var x = {1:2, 3:4,5:6, 5:6, 4:{6:8,9:0}};
-var y = {1:2, 3:4,5:6, 5:6, 4:{6:8,9:1}};
-deepcompare(x,y);
+deepcompare(1,2);
+deepcompare("a","b");
+deepcompare("a","a");
+deepcompare(1,1);
+deepcompare({1:2, 3:{2:5, 6:7}},{1:2, 4:{2:5, 6:8}});
